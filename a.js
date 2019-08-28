88ef5bbd79e5bc0bd9aa9175490fd8532918914b
ram-birla <52717008+ram-birla@users.noreply.github.com>
2019-08-28 22:08:13 +0530

    Update a.js

--- a.js
+++ a.js
@@ -4,10 +4,7 @@ var ginput = "fno"
 function selectinput(a)
 {
 ginput=a;
-}
 
-function clear(b){
-	document.getElementById(ginput).value = '';
 }
 
 function num(val) 
@@ -49,3 +46,56 @@ c= a / b;
 document.getElementById("result").value= c;
 }
 
+function per(){
+var a,b,c;
+a=Number(document.getElementById("fno").value);
+b=Number(document.getElementById("sno").value);
+c= (a*100)/b;
+document.getElementById("result").value= c;
+}
+
+function backSpace() {
+	var box = document.getElementById(ginput);  
+ 	var number = box.value;
+  
+  	var len = number.length - 1;
+  
+ 	var newNumber = number.substring( 0, len );
+  
+  	box.value = newNumber;
+
+} 
+
+function reset(){
+document.getElementById(ginput).value = '';
+}
+
+function square(){
+var a,b,c;
+if (ginput=="fno"){
+a=Number(document.getElementById(ginput).value);
+
+c= a * a;
+document.getElementById("result").value= c;
+   }
+else if (ginput=="sno"){
+b=Number(document.getElementById(ginput).value);
+c= b*b;
+document.getElementById("result").value= c;
+  }
+}
+
+function cube(){
+var a,b,c;
+if (ginput=="fno"){
+a=Number(document.getElementById(ginput).value);
+
+c= a * a *a;
+document.getElementById("result").value= c;
+   }
+else if (ginput=="sno"){
+b=Number(document.getElementById(ginput).value);
+c= b*b*b;
+document.getElementById("result").value= c;
+  }
+}
